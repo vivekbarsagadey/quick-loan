8fefd4bd63d32198c65187343d28da47006a644a
Vivek Barsagadey <vivek.bnb@gmail.com>
2018-06-27 22:22:56 +0530

    1) Added dockerized mysql.

---
+++ mysql/quick-loan.sql
@@ -0,0 +1,5 @@
+CREATE SCHEMA `dbquickloanadmin` ;
+CREATE SCHEMA `dbquickloanhyperledger` ;
+CREATE SCHEMA `dbquickloanservices` ;
+CREATE SCHEMA `dbquickloan` ;
+CREATE SCHEMA `dbquickloanuplportal` ;
