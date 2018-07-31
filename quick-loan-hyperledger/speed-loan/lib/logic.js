af28edf745f170d94c5e2f870aa9ffe682593eba
Prashant <prashantnagawade@gmail.com>
2018-07-31 12:46:08 +0530

    Fixed Hyperledger files

--- quick-loan-hyperledger/speed-loan/lib/logic.js
+++ quick-loan-hyperledger/speed-loan/lib/logic.js
@@ -1,153 +1,72 @@
 'use strict';
-var NS_assets = 'org.quickloan.assets';
-var NS_participants = 'org.quickloan.participants';
-var NS_transactions = 'org.quickloan.transactions';
+var NS_assets = 'org.whizit.pronto.assets';
+var NS_participants = 'org.whizit.pronto.participants';
+var NS_transactions = 'org.whizit.pronto.transactions';
 
 /**
- * @param {org.quickloan.transactions.createApplication} createApplication - create application for customer
+ * @param {org.whizit.pronto.transactions.requestInvestor} requestInvestor - send application to Investor
  * @transaction
  */
 
-function createApplication(createApplication) {
-  var applId = Math.random().toString(36).substring(3);
-  var newApplication = getFactory().newResource(NS_assets, 'Applications', applId);
-  newApplication.customer = createApplication.application.customer;
-  newApplication.applicationDate = createApplication.application.applicationDate;
-  newApplication.loanAmount = createApplication.application.loanAmount;
-  newApplication.applicationStatus = createApplication.application.applicationStatus;
-  newApplication.remarks = createApplication.application.remarks;
-  newApplication.customer.allApplications.push(newApplication);
-
-  return getAssetRegistry(NS_assets + '.Applications').then(function(ApplicationsRegistry) {
-    return ApplicationsRegistry.add(newApplication);
-  }).then(function() {
-  return getParticipantRegistry(NS_participants + '.Customer')}).then(function(CustomerRegistry) {
-    return CustomerRegistry.update(createApplication.application.customer);
-  });
-}
-
-/**
- * @param {org.quickloan.transactions.requestInvestor} requestInvestor - send application to investor
- * @transaction
- */
 function requestInvestor(requestInvestor) {
-  requestInvestor.investor.applicationRequests.applicationId = requestInvestor.application.applicationId;
-  requestInvestor.application.applicationStatus = "Application sent to Investor for approval";
-
-  return getParticipantRegistry(NS_participants + '.Investor')
-    .then(function(InvestorRegistry) {
-    	return InvestorRegistry.update(requestInvestor.investor);
-    })
-  	.then(function() {
-    	return getAssetRegistry(NS_assets + '.Applications');
-    })
-    .then(function(ApplicationsRegistry) {
-    	return ApplicationsRegistry.update(requestInvestor.application);
-  	})
-    .then(function() {
-    	return getParticipantRegistry(NS_participants + '.Customer')}).then(function(CustomerRegistry) {
-        	return CustomerRegistry.update(requestInvestor.application.customer);
-    });
-}
-
-/**
- * @param {org.quickloan.transactions.approveLoan} approveLoan - send application to investor
- * @transaction
- */
-function approveLoan(approveLoan) {
-  approveLoan.application.customer.lender.investorId = approveLoan.investor.investorId;
-  //var temp = approveLoan.investor.applicationAssigned.applicationId;
-  approveLoan.investor.applicationAssigned.applicationId = approveLoan.investor.applicationRequests.applicationId;
-  //approveLoan.investor.applicationRequests.applicationId = temp;
-  //approveLoan.investor.applicationRequests.applicationId = "NULL";
-  approveLoan.application.applicationStatus = "Application approved by Investor";
-
-  return getParticipantRegistry(NS_participants + '.Investor')
-    .then(function(InvestorRegistry) {
-    	return InvestorRegistry.update(approveLoan.investor);
-    })
-  	.then(function() {
-    	return getAssetRegistry(NS_assets + '.Applications');
-    })
-    .then(function(ApplicationsRegistry) {
-    	return ApplicationsRegistry.update(approveLoan.application);
-  	})
-    .then(function() {
-    	return getParticipantRegistry(NS_participants + '.Customer')
-    })
-    .then(function(CustomerRegistry) {
-        return CustomerRegistry.update(approveLoan.application.customer);
-    });
-}
-
-/**
- * @param {org.quickloan.transactions.rejectLoan} rejectLoan - send application to investor
- * @transaction
- */
-function rejectLoan(rejectLoan) {
-  rejectLoan.application.applicationStatus = "Application rejected by Investor";
-  rejectLoan.application.remarks = rejectLoan.remarks;
-
-  return getAssetRegistry(NS_assets + '.Applications')
-    .then(function(ApplicationsRegistry) {
-    	return ApplicationsRegistry.update(rejectLoan.application);
-    })
-    .then(function() {
-    	return getParticipantRegistry(NS_participants + '.Customer')
-    })
-    .then(function(CustomerRegistry) {
-        return CustomerRegistry.update(rejectLoan.application.customer);
-    });
-}
-
-/**
- * @param {org.quickloan.transactions.loanLent} loanLent - Change Loan Lent status
- * @transaction
- */
-
-function loanLent(loanLent) {
-  loanLent.application.applicationStatus = "Loan amount sent to customer";
-  loanLent.application.customer.lender.totalLoanLent = loanLent.application.loanAmount;
-
-  return getAssetRegistry(NS_assets + '.Applications')
-    .then(function(ApplicationsRegistry) {
-    	return ApplicationsRegistry.update(loanLent.application);
-  }).then(function() {
-  		return getParticipantRegistry(NS_participants + '.Customer')
-  })
-    .then(function(CustomerRegistry) {
-    	return CustomerRegistry.update(loanLent.application.customer);
-  })
-  .then(function() {
-  		return getParticipantRegistry(NS_participants + '.Investor')
-  })
-    .then(function(InvestorRegistry) {
-    	return InvestorRegistry.update(loanLent.application.customer.lender);
+  requestInvestor.application.status = "INVESTOR_REQUESTED";
+  requestInvestor.application.state = "IN_PROCESS";
+  requestInvestor.application.investor.id = requestInvestor.investor.id;
+  
+  return getAssetRegistry(NS_assets + '.Applications').then(function(ApplicationsRegistry) {
+    return ApplicationsRegistry.update(requestInvestor.application);
   });
 }
 
 /**
- * @param {org.quickloan.transactions.loanRepaid} loanRepaid - Change Loan Repaid status
+ * @param {org.whizit.pronto.transactions.updateScore} updateScore - Update credit score of Customer
  * @transaction
  */
 
-function loanRepaid(loanRepaid) {
-  loanRepaid.application.applicationStatus = "Loan amount repaid by the customer";
-  loanRepaid.application.customer.lender.totalLoanLent -= loanRepaid.application.loanAmount;
+function updateScore(updateScore) {
+  updateScore.customer.score += 2;
 
-  return getAssetRegistry(NS_assets + '.Applications')
-    .then(function(ApplicationsRegistry) {
-    return ApplicationsRegistry.update(loanRepaid.application);
-  })
-    .then(function() {
-  return getParticipantRegistry(NS_participants + '.Customer')})
-    .then(function(CustomerRegistry) {
-    return CustomerRegistry.update(loanRepaid.application.customer);
-  })
-  .then(function() {
-  		return getParticipantRegistry(NS_participants + '.Investor')
-  })
-    .then(function(InvestorRegistry) {
-    	return InvestorRegistry.update(loanRepaid.application.customer.lender);
+  return getParticipantRegistry(NS_participants + '.Customer').then(function(CustomerRegistry) {
+    return CustomerRegistry.update(updateScore.customer);
+  });
+}
+
+/**
+ * @param {org.whizit.pronto.transactions.updateCustomerStatus} updateCustomerStatus - Update Customer Status
+ * @transaction
+ */
+
+function updateCustomerStatus(updateCustomerStatus) {
+  updateCustomerStatus.customer.status = updateCustomerStatus.status;
+  
+  return getParticipantRegistry(NS_participants + '.Customer').then(function(CustomerRegistry) {
+    return CustomerRegistry.update(updateCustomerStatus.customer);
+  });
+}
+
+/**
+ * @param {org.whizit.pronto.transactions.updateInvestorStatus} updateInvestorStatus - Update Investor Status
+ * @transaction
+ */
+
+function updateInvestorStatus(updateInvestorStatus) {
+  updateInvestorStatus.investor.status = updateInvestorStatus.status;
+  
+  return getParticipantRegistry(NS_participants + '.Investor').then(function(InvestorRegistry) {
+    return InvestorRegistry.update(updateInvestorStatus.investor);
+  });
+}
+
+/**
+ * @param {org.whizit.pronto.transactions.updateApplicationStatus} updateApplicationStatus - Update Application Status
+ * @transaction
+ */
+
+function updateApplicationStatus(updateApplicationStatus) {
+  updateApplicationStatus.application.status = updateApplicationStatus.status;
+  updateApplicationStatus.application.remarks = updateApplicationStatus.remarks;
+  
+  return getAssetRegistry(NS_assets + '.Applications').then(function(ApplicationsRegistry) {
+    return ApplicationsRegistry.update(updateApplicationStatus.application);
   });
 }
